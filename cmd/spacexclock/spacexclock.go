41ecd008a38f8ee17cb9943cd93f981eee9f6c9e
Joshua Adams <n7down@gmail.com>
2019-06-24 09:15:15 -0600

    added timer and other metrics

--- cmd/spacexclock/spacexclock.go
+++ cmd/spacexclock/spacexclock.go
@@ -18,6 +18,8 @@ import (
 	"fmt"
 	"github.com/n7down/Displays/internal/spacexapi"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
 	"strings"
 	"time"
 )
@@ -53,28 +55,44 @@ func main() {
 	rocketTypeCamelCase := rocket.Engines.Type
 	rocketTypeCamelCase = strings.ToUpper(string(rocketTypeCamelCase[0])) + rocketTypeCamelCase[1:]
 
-	timeNow := time.Now().UTC().Format("Mon Jan _2, 2006 15:04:05")
-	nextLaunchTimeUtc := nextLaunch.LaunchDateUtc
-	nextLaunchTimeUtcFormated := nextLaunchTimeUtc.Format("Mon Jan _2, 2006 15:04:05 ")
+	refreshTime := time.Now()
 
-	fmt.Println("____ ___  ____ ____ ____ _  _")
-	fmt.Printf("[__  |__] |__| |    |___  \\/   \tSpaceX API: \t[v%s]\n", spaceXApiVersion)
-	fmt.Printf("___] |    |  | |___ |___ _/\\_  \tVersion: \t[v%s]\n", spaceXClockVersion)
-	fmt.Println()
-	fmt.Println("TIME ========================================================")
-	fmt.Printf(" Time Now UTC: \t\t\t%s\n", timeNow)
-	fmt.Println("LAUNCH ======================================================")
-	fmt.Printf(" Mission Name: \t\t\t%s\n", nextLaunch.MissionName)
-	fmt.Printf(" Flight Number: \t\t%d\n", nextLaunch.FlightNumber)
-	fmt.Printf(" Launch Site: \t\t\t%s\n", nextLaunch.LaunchSite.SiteName)
-	fmt.Printf(" Launch Time UTC: \t\t%s\n", nextLaunchTimeUtcFormated)
-	fmt.Printf(" Elapsed Time: \t\t\t%s\n", time.Since(nextLaunchTimeUtc))
+	for {
 
-	// TODO: show graph of elapsed time - show elapsed time after elapsed time is < 24 hours
-	fmt.Print(" [\t\t\t\t\t\t\t]\n")
-	fmt.Println("ROCKET =====================================================")
-	fmt.Printf(" Rocket Name: \t\t\t%s\n", nextLaunch.Rocket.RocketName)
-	fmt.Printf(" Engines: \t\t\t%d\n", rocket.Engines.Number)
-	fmt.Printf(" Name: \t\t\t\t%s\n", rocketTypeCamelCase)
-	fmt.Printf(" Version: \t\t\t%s\n", rocket.Engines.Version)
+		elapsedRefreshTime := time.Since(refreshTime)
+		if elapsedRefreshTime > time.Second {
+			refreshTime = time.Now()
+
+			// FIXME: how do I do this without eating the cpu
+			cmd := exec.Command("clear")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+
+			timeNow := time.Now().UTC().Format("Mon Jan _2, 2006 15:04:05")
+			nextLaunchTimeUtc := nextLaunch.LaunchDateUtc
+			nextLaunchTimeUtcFormated := nextLaunchTimeUtc.Format("Mon Jan _2, 2006 15:04:05 ")
+
+			fmt.Println("____ ___  ____ ____ ____ _  _")
+			fmt.Printf("[__  |__] |__| |    |___  \\/   \tSpaceX API: \t[v%s]\n", spaceXApiVersion)
+			fmt.Printf("___] |    |  | |___ |___ _/\\_  \tVersion: \t[v%s]\n", spaceXClockVersion)
+			fmt.Println()
+			fmt.Println("SYSTEM ========================================================")
+			fmt.Printf(" IPv4: \t\t\t\t%s", "192.168.0.20\n")
+			fmt.Printf(" Time Now UTC: \t\t\t%s\n", timeNow)
+			fmt.Println("LAUNCH ======================================================")
+			fmt.Printf(" Mission Name: \t\t\t%s\n", nextLaunch.MissionName)
+			fmt.Printf(" Flight Number: \t\t%d\n", nextLaunch.FlightNumber)
+			fmt.Printf(" Launch Site: \t\t\t%s\n", nextLaunch.LaunchSite.SiteName)
+			fmt.Printf(" Launch Time UTC: \t\t%s\n", nextLaunchTimeUtcFormated)
+			fmt.Printf(" Elapsed Time: \t\t\t%s\n", time.Since(nextLaunchTimeUtc))
+
+			// TODO: show graph of elapsed time - show elapsed time after elapsed time is < 24 hours
+			fmt.Print(" [\t\t\t\t\t\t\t]\n")
+			fmt.Println("ROCKET =====================================================")
+			fmt.Printf(" Rocket Name: \t\t\t%s\n", nextLaunch.Rocket.RocketName)
+			fmt.Printf(" Engines: \t\t\t%d\n", rocket.Engines.Number)
+			fmt.Printf(" Name: \t\t\t\t%s\n", rocketTypeCamelCase)
+			fmt.Printf(" Version: \t\t\t%s\n", rocket.Engines.Version)
+		}
+	}
 }
