0e20e7de6202e58b644db41900c0134831a4ccb4
Joshua Adams <n7down@gmail.com>
2019-06-30 20:01:07 -0600

    updated to run every second

--- cmd/pitftdisplay/pitftdisplay.go
+++ cmd/pitftdisplay/pitftdisplay.go
@@ -3,19 +3,24 @@ package main
 import (
 	"github.com/n7down/PITFTDisplays/internal/config"
 	"github.com/n7down/PITFTDisplays/internal/display"
-	//"github.com/n7down/PITFTDisplays/internal/display/spacexdisplay"
-	"github.com/n7down/PITFTDisplays/internal/display/githubdisplay"
+	"github.com/n7down/PITFTDisplays/internal/display/spacexdisplay"
+	"time"
+	//"github.com/n7down/PITFTDisplays/internal/display/githubdisplay"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	c, err := config.Config()
+	_, err := config.Config()
 	if err != nil {
 		log.Error(err)
 	}
 
 	displayManager := display.NewDisplayManager()
-	//displayManager.AddDisplay(spacexdisplay.NewSpaceXDisplay())
-	displayManager.AddDisplay(githubdisplay.NewGithubDisplay(c))
-	displayManager.Render()
+	displayManager.AddDisplay(spacexdisplay.NewSpaceXDisplay())
+	//displayManager.AddDisplay(githubdisplay.NewGithubDisplay(c))
+	// TODO: render every second
+	for {
+		time.Sleep(time.Second)
+		displayManager.Render()
+	}
 }
