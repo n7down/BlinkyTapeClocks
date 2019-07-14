d1b96338674c1e26e5f6f23c7eeb7a0d030454f1
Joshua Adams <n7down@gmail.com>
2019-07-13 19:57:45 -0600

    updating display

--- internal/display/spacexdisplay/spacexdisplay.go
+++ internal/display/spacexdisplay/spacexdisplay.go
@@ -6,8 +6,6 @@ import (
 	"github.com/n7down/timelord/internal/spacexapi"
 	"github.com/n7down/timelord/internal/utils"
 
-	//aurora "github.com/logrusorgru/aurora"
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"strings"
 	"time"
@@ -36,7 +34,7 @@ func NewSpaceXDisplay(config *viper.Viper) (*SpaceXDisplay, error) {
 		return nil, err
 	}
 
-	// TODO: set the refresh time to the next rocket launch time +1 day
+	// TODO: set the refresh time to the next rocket launch time +1 week
 
 	d := SpaceXDisplay{
 		LastUpdate: time.Now(),
@@ -58,39 +56,14 @@ func (s SpaceXDisplay) Render() string {
 	var buffer bytes.Buffer
 
 	rocketType := strings.Title(s.Rocket.Engines.Type)
-
-	ipAddress, err := utils.GetLocalIP()
-	if err != nil {
-		log.Error(err)
-	}
-
-	hostname, err := utils.GetHostName()
-	if err != nil {
-		log.Error(err)
-	}
-
 	timeNow := time.Now().Format("Mon Jan _2, 2006 15:04:05")
 	timeNowUTC := time.Now().UTC().Format("Mon Jan _2, 2006 15:04:05")
 	nextLaunchTimeUtc := s.NextLaunch.LaunchDateUtc
 	nextLaunchTimeUtcFormated := nextLaunchTimeUtc.Format("Mon Jan _2, 2006 15:04:05 ")
 	elapsedTime := utils.NewElapsedTime(time.Until(nextLaunchTimeUtc))
 
-	// FIXME: get the pitftversion from the config
-	//piTftVersion := ""
-	piTftVersion := s.config.GetString("version")
-
-	//fmt.Println("")
-	//fmt.Println("     ███████╗██████╗  █████╗  ██████╗███████╗██╗  ██╗")
-	//fmt.Println("     ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝╚██╗██╔╝")
-	//fmt.Println("     ███████╗██████╔╝███████║██║     █████╗   ╚███╔╝ ")
-	//fmt.Println("     ╚════██║██╔═══╝ ██╔══██║██║     ██╔══╝   ██╔██╗")
-	//fmt.Println("     ███████║██║     ██║  ██║╚██████╗███████╗██╔╝ ██╗")
-	//fmt.Println("     ╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝╚═╝  ╚═╝")
-
 	buffer.WriteString("\n")
-	buffer.WriteString(fmt.Sprintf(" SPACEX: [v%s]\n", spaceXApiVersion))
-	buffer.WriteString(fmt.Sprintf(" PITFTDisplay: [%s]\n", piTftVersion))
-	buffer.WriteString(fmt.Sprintf(" Hostname: [%s] \t\tIPv4: [%s/24]\n", hostname, ipAddress))
+	buffer.WriteString(fmt.Sprintf(" SPACEX [v%s]\n", spaceXApiVersion))
 	buffer.WriteString("\n")
 	buffer.WriteString(" MISSION --------------------------------------------------\n")
 	buffer.WriteString(fmt.Sprintf("  Name: %s \t\t\tFlight Number: %d\n", s.NextLaunch.MissionName, s.NextLaunch.FlightNumber))
